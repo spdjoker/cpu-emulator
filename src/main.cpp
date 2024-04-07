0f014049a780a76643099495e8d77293bd6f0153
Jose Gonzalez Lopez <joseg072103@gmail.com>
2024-04-07 14:27:19 -0400

    removed imgui submodule

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
-#include "SDL3/SDL.h"
 #include "fmt/base.h"
 #include "jkr/cmd_options.hpp"
-#include "jkr/compiler.hpp"
-#include "jkr/hardware/cpu.hpp"
-#include "sim_program.hpp"
-#include <fmt/format.h>
 
 int cmd(int argc, char* argv[]) {
   CMD_Options opts(argc, argv);
@@ -12,17 +7,11 @@ int cmd(int argc, char* argv[]) {
   if (!opts.success)
     return -1;
 
-  if (opts.flags & CMD_Options::FLAG_COMPILE) {
-    compile(opts.c_val, NULL);
-  }
   return 0;
 }
 
 int main(int argc, char* argv[]) {
   fmt::println("YO");
-
-  SimProgram program(800, 600);
-  program.execute();
 
   return 0;
 } 
